7dd942fa118f102b4e95757732cb3f4f664e3ba7
wongprom <>
2024-08-05 13:22:25 +0200

    Add testData with interface.

---
+++ src/data.ts
@@ -0,0 +1,25 @@
+import { ITodoData } from './interfaces';
+
+export const todosData: ITodoData[] = [
+  {
+    author: 'Jimmy',
+    id: '1',
+    isCompleted: false,
+    todo: 'walk the dog',
+    createdAt: '2015-12-12',
+  },
+  {
+    author: 'Sandra',
+    id: '2',
+    isCompleted: false,
+    todo: 'Go fish',
+    createdAt: '2016-10-10',
+  },
+  {
+    author: 'Patrik',
+    id: '3',
+    isCompleted: false,
+    todo: 'Go fish',
+    createdAt: '2016-10-10',
+  },
+];
---
+++ src/interfaces.ts
@@ -0,0 +1,7 @@
+export interface ITodoData {
+  author: string;
+  id: string;
+  isCompleted: boolean;
+  todo: string;
+  createdAt: string;
+}
