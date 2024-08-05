0d288c033dea6cf68a10d8b0f9fc2337465c75e0
wongprom <>
2024-08-05 14:31:41 +0200

    Create input and log value onClick in AddTodo.

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,3 +1,26 @@
+import { MouseEventHandler, useState } from 'react';
+
 export const AddTodo = () => {
-  return <>AddTodo</>;
+  const [input, setInput] = useState<string>('');
+
+  const handelSubmit: MouseEventHandler<HTMLButtonElement> = (e) => {
+    e.preventDefault();
+    // todo Validate input
+    console.log('input: ', input);
+  };
+
+  return (
+    <>
+      <form>
+        <label htmlFor="input">Add new todo</label>
+        <input
+          id="input"
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button onClick={handelSubmit}>Add Todo</button>
+      </form>
+    </>
+  );
 };
