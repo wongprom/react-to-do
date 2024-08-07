baeeb5d60738a0280e5457d407a5f0141e36be41
wongprom <jimmybackstrom@hotmail.com>
2024-08-07 17:15:40 +0200

    Styling: AddTodoPage from screen 375px >

--- src/pages/AddTodoPage.tsx
+++ src/pages/AddTodoPage.tsx
@@ -1,11 +1,11 @@
-import { ChangeEventHandler, MouseEventHandler, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { v4 as uuidv4 } from 'uuid';
+import { ChangeEventHandler, MouseEventHandler, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
 
-import { useTodoContext } from '../customHooks';
-import { initialFormData } from '../data';
-import { isFormInputsValid } from '../utils';
-import { ITodo } from '../interfaces';
+import { useTodoContext } from "../customHooks";
+import { initialFormData } from "../data";
+import { isFormInputsValid } from "../utils";
+import { ITodo } from "../interfaces";
 
 export const AddTodoPage = () => {
   const [formData, setFormData] = useState<ITodo>(initialFormData);
@@ -18,7 +18,7 @@ export const AddTodoPage = () => {
     if (isFormInputsValid(formData)) {
       addTodo(newTodo);
       setFormData(initialFormData);
-      navigate('/');
+      navigate("/");
     }
   };
 
@@ -31,26 +31,37 @@ export const AddTodoPage = () => {
   };
 
   return (
-    <>
-      <form>
-        <label htmlFor="author">Author</label>
-        <input
-          id="author"
-          name="author"
-          type="text"
-          value={formData.author}
-          onChange={handleInputOnChange}
-        />
-        <label htmlFor="todo">New todo</label>
-        <input
-          id="todo"
-          name="todo"
-          type="text"
-          value={formData.todo}
-          onChange={handleInputOnChange}
-        />
-        <button onClick={(e)=>handelSubmit(e)}>Add Todo</button>
+    <div className="add-todo-page">
+      <h1>Add New Todo</h1>
+      <form className="form">
+        <div className="input-wrapper">
+          <label htmlFor="author">Author</label>
+          <input
+            className="border-radius-20"
+            id="author"
+            name="author"
+            onChange={handleInputOnChange}
+            type="text"
+            value={formData.author}
+          />
+        </div>
+        <div className="input-wrapper">
+          <label htmlFor="todo">New todo</label>
+          <input
+            className="border-radius-20"
+            id="todo"
+            name="todo"
+            onChange={handleInputOnChange}
+            type="text"
+            value={formData.todo}
+          />
+        </div>
+        <div className="button-wrapper">
+          <button onClick={(e) => handelSubmit(e)}>
+            <strong>Add Todo</strong>
+          </button>
+        </div>
       </form>
-    </>
+    </div>
   );
 };
