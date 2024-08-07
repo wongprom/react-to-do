d73e5089c419094ae18718e0e13042e4f3558605
wongprom <jimmybackstrom@hotmail.com>
2024-08-07 18:35:21 +0200

    Styling: make AddTodoPage look good on all screen sizes.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,16 +4,18 @@ import { navLinks } from "../data";
 export const Header = () => {
   return (
     <header className="header">
-      <h1 className="logo">The Movie List</h1>
-      <div className="links">
-        {navLinks.map((link) => (
-          <NavLink
-            to={link.to}
-            className={({ isActive }) => (isActive ? "link active" : "link")}
-          >
-            {link.text}
-          </NavLink>
-        ))}
+      <div className="header-wrapper">
+        <h1 className="logo">The Movie List</h1>
+        <div className="links">
+          {navLinks.map((link) => (
+            <NavLink
+              to={link.to}
+              className={({ isActive }) => (isActive ? "link active" : "link")}
+            >
+              {link.text}
+            </NavLink>
+          ))}
+        </div>
       </div>
     </header>
   );
